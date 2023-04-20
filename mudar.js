71b53335562f893ca31460f4c6c6c22678427636
s0pa-fria <130664417+s0pa-fria@users.noreply.github.com>
2023-04-20 14:35:19 -0300

    Atualização- Imagens

--- mudar.js
+++ mudar.js
@@ -1,4 +1,15 @@
-//  
+// Fazer esta função 2º (Ela troca a imagem do sorvete grande)
+function imgSlider(anything){
+    document.querySelector('.starbucks').src = anything;
+}
+
+// Fazer esta função 3º (Ela troca a cor do circulo)
+function changeCircleColor(color){
+    const circulo =document.querySelector('.circulo');
+    circulo.style.background =color; 
+}
+
+// Fazer esta função 1º (Ela altera  o estado Ativado/Desativado)  
 function toggleMenu() {
     var menuToggle = document.querySelector('.toggle');/*Cria uma variável e seleciona classe (.toggle)*/
     var links =document.querySelector('.links');/*Cria uma variável e seleciona classe (.links)*/
